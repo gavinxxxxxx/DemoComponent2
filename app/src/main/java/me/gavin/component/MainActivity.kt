cf34e295c4f4e775a0e20d302c8c0f7c56f792e4
gavinx <gavin.xxx.xxx+git@gmail.com>
2020-03-11 18:17:25 +0800

    .

---
+++ app/src/main/java/me/gavin/component/MainActivity.kt
@@ -0,0 +1,16 @@
+package me.gavin.component
+
+import android.os.Bundle
+import me.gavin.base.BaseActivity
+
+class MainActivity : BaseActivity() {
+
+    override fun setContentView() {
+
+    }
+
+    override fun afterCreate(savedInstanceState: Bundle?) {
+
+    }
+
+}
